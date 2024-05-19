102427346857f210fabefc5b4bcda8442c601517
arrazy100 <afdhalarrazy111@gmail.com>
2024-05-19 12:03:26 +0700

    Fix responsive mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 		get_character_name_by_id,
 		get_character_splash
 	} from '../utils/util';
+	import { onMount, onDestroy } from 'svelte';
 
 	let characters: Character[] = [];
 	let characterDB = get_character_list();
@@ -75,10 +76,10 @@
 
 	const outputCommand = () => {
 		let commandString = '';
-		characters.forEach(character => {
+		characters.forEach((character) => {
 			character.relics.forEach((relic, index) => {
 				let substats_str = '';
-				relic.substats.forEach(substat => {
+				relic.substats.forEach((substat) => {
 					substats_str += ` ${substat.id} ${substat.rolls + 1}`;
 				});
 
@@ -95,70 +96,127 @@
 		link.download = 'relics.txt';
 
 		link.click();
+	};
+
+	let menuOpen = false;
+
+	function handleClickOutside(event: any) {
+		const menu = document.querySelector('.menu');
+		const toggleBtn = document.querySelector('.toggle-btn');
+		if (
+			menu &&
+			toggleBtn &&
+			!menu.contains(event.target) &&
+			!toggleBtn.contains(event.target)
+		) {
+			menuOpen = false;
+		}
 	}
+
+	onMount(() => {
+		document.addEventListener('click', handleClickOutside);
+	});
+
+	onDestroy(() => {
+		document.removeEventListener('click', handleClickOutside);
+	});
 </script>
 
 <main>
 	<input type="file" class="hidden" bind:this={fileInput} on:change={(e) => handleFileInput(e)} />
-	<div class="bg-slate-950 w-full h-screen grid grid-cols-3">
-		<div class="col-span-1 overflow-y-auto w-full">
-			<div class="w-full flex flex-col h-screen items-center p-10">
-				<div class="text-white mb-10 text-2xl font-bold">YET ANOTHER RELIC SETUP</div>
+	<div class="bg-slate-950 w-full h-screen relative">
+		<!-- EXPAND MENU SECTION -->
+		<button
+			class="toggle-btn absolute top-4 left-4 px-4 py-2 bg-slate-950 text-white font-bold rounded-lg shadow-md hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+			style="visibility: {menuOpen ? 'hidden' : 'visible'}"
+			on:click={() => (menuOpen = !menuOpen)}
+		>
+			<i class="fas fa-angles-right"></i>
+		</button>
+		<!-- END OF EXPAND MENU SECTION -->
 
-				<div class="w-full">
-					{#each characters as character, index}
-						<div class="my-2">
-							<button
-								on:click={() => changeCharacter(index)}
-								class="block w-full mt-1 py-2 px-3 border text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+		<!-- MENU SECTION -->
+		<div
+			class="menu z-10 absolute top-0 left-0 h-full bg-slate-900 transition-transform transform {menuOpen
+				? 'translate-x-0'
+				: '-translate-x-full'}"
+		>
+			<button
+				class="absolute top-4 right-4 px-4 py-2 bg-slate-900 text-white font-bold rounded-lg shadow-md hover:bg-slate-950 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+				on:click={() => (menuOpen = false)}
+			>
+				<i class="fas fa-times"></i>
+			</button>
+			<div class="overflow-y-auto w-full h-fit p-4 mt-10">
+				<div class="w-full flex flex-col items-center p-4">
+					<div class="text-white mb-4 text-2xl font-bold text-center">YET ANOTHER RELIC SETUP</div>
+					<div class="w-full">
+						{#each characters as character, index}
+							<div class="my-2">
+								<button
+									on:click={() => changeCharacter(index)}
+									class="block w-full py-2 px-3 border text-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+								>
+									{character.name}
+								</button>
+							</div>
+						{/each}
+					</div>
+					<div class="w-full mt-4">
+						<div class="mb-2">
+							<select
+								bind:value={characterId}
+								class="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
 							>
-								{character.name}
+								{#each characterDB as character}
+									<option value={character.id}>{character.name}</option>
+								{/each}
+							</select>
+						</div>
+						<div class="mb-2">
+							<button
+								class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+								on:click={() => addCharacter(characterId)}
+							>
+								Add Character
 							</button>
 						</div>
-					{/each}
-				</div>
-
-				<div class="w-full">
-					<div class="mb-2">
-						<select
-							bind:value={characterId}
-							class="block w-full mt-1 py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-						>
-							{#each characterDB as character}
-								<option value={character.id}> {character.name} </option>
-							{/each}
-						</select>
-					</div>
-					<div class="mb-2">
-						<button
-							class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-							on:click={() => addCharacter(characterId)}>Add Character</button
-						>
-					</div>
-					<div class="mb-2">
-						<button
-							class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-							on:click={fileInput.click()}>Import Data</button
-						>
-					</div>
-					<div class="mb-2">
-						<button
-							class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-							on:click={() => exportData()}>Export Data</button
-						>
-					</div>
-					<div class="mb-10">
-						<button
-							class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-							on:click={() => outputCommand()}
-							>Output Command</button
-						>
+						<div class="mb-2">
+							<button
+								class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+								on:click={() => fileInput.click()}
+							>
+								Import Data
+							</button>
+						</div>
+						<div class="mb-2">
+							<button
+								class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+								on:click={() => exportData()}
+							>
+								Export Data
+							</button>
+						</div>
+						<div class="mb-10">
+							<button
+								class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+								on:click={() => outputCommand()}
+							>
+								Output Command
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- Character Data -->
-		<div class="bg-slate-900 shadow-lg col-span-2 overflow-y-auto p-10">
+		<!-- END OF MENU SECTION -->
+
+		<!-- CHARACTER DATA SECTION -->
+		<div
+			class="transition-all {menuOpen
+				? 'ml-72'
+				: 'ml-0'} bg-slate-950 shadow-lg overflow-y-auto overflow-x-auto p-10 h-full"
+		>
 			{#if characterIndex != null}
 				<div class="flex flex-col">
 					<div class="flex w-full flex-col justify-center items-center mb-10">
@@ -177,9 +235,9 @@
 				</div>
 			{/if}
 		</div>
+		<!-- END OF CHARACTER DATA SECTION -->
 	</div>
 </main>
 
 <style>
-	/* Add styling here */
 </style>
